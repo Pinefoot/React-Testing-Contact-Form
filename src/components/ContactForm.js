c8b9fb97d1abaafe7cf9da3ad397165fa68b9164
Pinefoot <jameslcarpino@gmail.com>
2020-04-09 16:04:20 -0700

    finishing up the MVP requirements.

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -14,8 +14,9 @@ const ContactForm = () => {
     <div className="App">
       <form onSubmit={handleSubmit(onSubmit)}>
         <div>
-          <label htmlFor="firstName">First Name*</label>
+          <label htmlFor="firstName" data-testid="firstName">First Name*</label>
           <input
+            id = 'firstName'
             name="firstName"
             placeholder="bill"
             ref={register({ required: true, maxLength: 3 })}
@@ -28,6 +29,7 @@ const ContactForm = () => {
         <div>
           <label htmlFor="lastName">Last Name*</label>
           <input
+            id = "lastName"
             name="lastName"
             placeholder="luo"
             ref={register({ required: true })}
@@ -41,14 +43,14 @@ const ContactForm = () => {
           <label htmlFor="email" placeholder="bluebill1049@hotmail.com">
             Email*
           </label>
-          <input name="email" ref={register({ required: true })} />
+          <input id = "email" name="email" ref={register({ required: true })} />
           {errors.email && (
             <p>Looks like there was an error: {errors.email.type}</p>
           )}
         </div>
         <div>
-          <label htmlFor="message">Message</label>
-          <textarea name="message" ref={register({ required: false })} />
+          <label htmlFor="message" >Message</label>
+          <textarea name="message" id="message" ref={register({ required: false })} />
         </div>
         {data && (
           <pre style={{ textAlign: "left", color: "white" }}>
